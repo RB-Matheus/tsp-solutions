185d195abc5b0594af7add600b478e2bad1fbb46
Matheus <76456871+RB-Matheus@users.noreply.github.com>
2024-11-19 13:48:14 -0300

    Modified the reading logic

--- tsp-cormen/src/main.c
+++ tsp-cormen/src/main.c
@@ -1,40 +1,28 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "../include/grafo.h"
 
 
 int main(void) {
     int u, v, r, qtd_vertices;
-    double peso;
+    double xu, yu, xv, yv;
 
     fscanf(stdin, "%d %d", &qtd_vertices, &r);
-    Grafo *g = criar_grafo(qtd_vertices);
 
-    while (fscanf(stdin, "%d %d %lf", &u, &v, &peso) != EOF) {
-        inserir_aresta(g, u, v, peso);
+    Grafo *g = criar_grafo(qtd_vertices);
+    assert(g != NULL);
+
+    Vertice **H = alocar_ciclo(g->qtd_vertices);
+    assert(H != NULL);
+
+    while (fscanf(stdin, "%d %lf %lf %d %lf %lf", &u, &xu, &yu, &v, &xv, &yv) != EOF) {
+            inserir_aresta(g, u, v, sqrt(pow(xv - xu, 2) + pow(yv - yu, 2)));
     }
 
     prim(g, r);
-
-    //exibir_lista_adjacencia(g);
-    //exibir_resumo(g);
-
-//Vértice  | Chave (d)                  | Pai (pi)
-//---------|----------------------------|---------
-//0        | 0.000000                   | -1      
-//1        | 2.000000                   | 0       
-//2        | 1.414214                   | 1       
-//3        | 1.414214                   | 4       
-//4        | 1.414214                   | 5       
-//5        | 2.000000                   | 1       
-//6        | 1.414214                   | 4       
-//7        | 2.236068                   | 1
-// Peso total: 11.892922
-
-    Vertice **H = alocar_ciclo(g->qtd_vertices);
     double custo_total;
-    // testar se H != NULL
     realizar_percurso(g, r, H, &custo_total);
     exibir_resumo(g);
     exibir_ciclo(g, H, &custo_total);
@@ -54,4 +42,4 @@ int main(void) {
 // gcc src/main.c src/grafo.c src/heap_min_fp.c -o bin/m -Iinclude -lm -Wall -Wextra
 
 // < COMO EXECUTAR >
-// cat tests/test3.graph | bin/m
+// cat tests/output400.graph | bin/m
